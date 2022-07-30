9820530d441c81d7612ff97ed0d5efe2173d6122
JuanQueso <102824078+JuanQueso@users.noreply.github.com>
2022-07-30 11:30:44 +0200

    Nuget Package restore and updates

---
+++ src/CoMute/Migrations/Configuration.cs
@@ -0,0 +1,20 @@
+﻿namespace CoMute.Web.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using CoMute.Web.Models.Dto;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<RegistrationRequest>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(RegistrationRequest context)
+        {
+        }
+    }
+}
